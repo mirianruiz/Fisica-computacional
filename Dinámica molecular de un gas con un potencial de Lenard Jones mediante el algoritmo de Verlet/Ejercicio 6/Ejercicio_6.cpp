fda8de56ae10d32da05fcbf4bccbe13f82a95db1
mirianruiz <mirianrm@correo.ugr.es>
2026-07-10 19:37:54 +0200

    Ejercicio 6
    Evolución de la fluctuaciones de la posición de una partícula cualquiera.

---
+++ Dinámica molecular de un gas con un potencial de Lenard Jones mediante el algoritmo de Verlet/Ejercicio 6/Ejercicio_6.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+void aceleracion(const vector < double > &x, const vector < double > &y, vector < double > &a_x, vector < double > &a_y, double L, int N);
+void algoritmo_verlet(vector < double > &x, vector < double > &y, vector < double > &v_x, vector < double > &v_y, double L, int N);
+
+int main()
+{
+
+
+    // Semilla para el generador de números aleatorios basada en el tiempo actual
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();  
+    mt19937_64 generator(seed);  // Inicializo el generador de números aleatorios
+
+    double L = 12.0; //Tamaño del lado de la caja cuadrada
+    int N = 144; //Número de átomos dentro de la caja
+    
+    //**********************Posiciones iniciales de los átomos********************************/
+    
+   // Red cuadrada equiespaciada
+    vector < double > x(N), y(N); 
+
+    int numero_lados = sqrt(N);
+
+    int i = 0;
+    for (int fila = 0; fila < numero_lados; fila++)
+    {
+        for (int columna = 0; columna < numero_lados; columna++)
+        {
+            x[i] = columna;
+            y[i] = fila;
+            
+            i++;
+        }
+    }
+
+ 
+    //**********************Velocidades iniciales de los átomos********************************/
+
+    // Genero los arrays de las velocidades nulas
+
+    vector < double > v_x(N, 0.0), v_y(N, 0.0);
+
+    //Llamo al algoritmo de Verlet para evolucionar el sistema de átomos en el tiempo
+
+    algoritmo_verlet(x, y, v_x, v_y, L, N);
+
+
+
+    return 0;
+}
+
+
+// ********************Función para el Algoritmo de Verlet********************
+
+void algoritmo_verlet(vector < double > &x, vector < double > &y, vector < double > &v_x, vector < double > &v_y, double L, int N) 
+{
+    //**************************Paso 0. Calculo h y t*************************"""
+    double t = 0.0;
+    double h = 0.002;
+
+    //****************Paso 1. Calculo la aceleración inicial de cada átomo a partir de las posiciones iniciales**************
+
+    vector < double > a_x(N, 0.0), a_y(N, 0.0);
+    aceleracion(x, y, a_x, a_y, L, N);
+
+
+
+    //Guardo las posiciones y velocidades de los átomos en ficheros, de forma que cada fila 
+    // corresponda a un tiempo t, y de dos en dos columnas corresponda a un átomo
+    //de forma que se escriba con x1 y1 x2 y2... y lo mismo con las velocidades.
+
+    ofstream archivo_posiciones("Posiciones_atomos_ej_6.txt");
+    ofstream archivo_velocidades("Velocidades_atomos_ej_6.txt");
+    ofstream archivo_posicion_atomo_sin_c_c("Posicion_Atomo_sin_condiciones_periodicas_ej_6.txt");
+
+    // Genero un array para guardar las posiciónes del los átomos sin aplicar las condiciones periódicas 
+    vector<double> x_sin_c_c=x;
+    vector<double> y_sin_c_c=y;
+
+   
+
+    for (int i = 0; i < N; i++) 
+    {
+        archivo_posiciones << x[i] << ' ' << y[i] << ' ';
+        archivo_velocidades << v_x[i] << ' ' << v_y[i] << ' ';
+        archivo_posicion_atomo_sin_c_c << x_sin_c_c[i] << ' ' << y_sin_c_c[i] << ' ';
+    }
+    archivo_posiciones << '\n';
+    archivo_velocidades << '\n';
+    archivo_posicion_atomo_sin_c_c << '\n';
+
+    
+   
+    //Quiero evolucionar el sistema hasta t=500, si el paso temporal es h=0.002,
+    //el número de pasos temporales 
+
+      int P= int(500 / h);
+
+    // Inicio el ciclo temporal
+    for (int k = 1; k <= P; k++) 
+    {
+       //************************paso 2. Calculo wi  y la posición en t+h********************** """
+
+        vector < double > w_x(N), w_y(N);
+
+        for (int i = 0; i < N; i++) 
+        {
+            w_x[i] = v_x[i] + (h / 2.0) * a_x[i];
+            w_y[i] = v_y[i] + (h / 2.0) * a_y[i];
+        }
+
+        for (int i = 0; i < N; i++) 
+        {
+            x[i] = x[i] + h * w_x[i];
+            y[i] = y[i] + h * w_y[i];
+            
+            //Tratectoria real de los átomos sin aplicar condiciones periódicas
+
+            x_sin_c_c[i] = x_sin_c_c[i] + h * w_x[i];
+            y_sin_c_c[i] = y_sin_c_c[i] + h * w_y[i];
+        }
+
+
+        // Se ha de tener en cuenta las condiciones periódicas al calcular las nuevas posiciones de los átomos
+        for (int i = 0; i < N; i++) {
+            if (x[i] < 0.0) x[i] += L;
+            if (x[i] >= L) x[i] -= L;
+            if (y[i] < 0.0) y[i] += L;
+            if (y[i] >= L) y[i] -= L;
+        }
+        
+        //"""*********************Paso 3. Calculo la aceleración en t+h haciendo uso de las posiciones en t+h calculadas arriba justo*****"""
+
+        for (int i = 0; i < N; i++) {
+            a_x[i] = 0.0;
+            a_y[i] = 0.0;
+        }
+
+        aceleracion(x, y, a_x, a_y, L, N);
+
+        //****************************Paso 4. Calculo la velocidad en t+h**********************
+    
+        for (int i = 0; i < N; i++) 
+        {
+            v_x[i] = w_x[i] + (h / 2) * a_x[i];
+            v_y[i] = w_y[i] + (h / 2) * a_y[i];
+        }
+
+        //********************Paso 5. Sumo al tiempo h******************************************
+
+        t =t + h;
+
+        // Guardo las posiciones y velocidades en  los ficheros
+
+        for (int i = 0; i < N; i++) 
+        {
+            archivo_posiciones << x[i] << ' ' << y[i] << ' ';
+            archivo_velocidades << v_x[i] << ' ' << v_y[i] << ' ';
+            archivo_posicion_atomo_sin_c_c << x_sin_c_c[i] << ' ' << y_sin_c_c[i] << ' ';
+        }
+        archivo_posiciones << '\n';
+        archivo_velocidades << '\n';
+        archivo_posicion_atomo_sin_c_c << '\n';
+    
+
+        // Reescalo las velocidades para aumentar la temperatura en t = 180, 270, 300 y 400 segundos
+        if ((k == 90000) || (k == 135000) || (k == 150000) || (k == 200000))
+        {
+            for (int i = 0; i < N; i++) 
+            {
+                v_x[i] *= 1.5;
+                v_y[i] *= 1.5;
+            }
+        }
+    }
+
+    archivo_posiciones.close();
+    archivo_velocidades.close();
+    archivo_posicion_atomo_sin_c_c.close();
+}
+
+
+
+
+// ********************Función para el calculo de la aceleración inicial de cada átomo********************
+
+
+void aceleracion(const vector < double > &x, const vector < double > &y, vector < double > &a_x, vector < double > &a_y, double L, int N) {
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = i + 1; j < N; j++) // si  j = i + 1, no se calcula dos veces la interacción entre el átomo i y el j, por la ley de acción y reacción
+        {
+            // Condiciones periódicas de la caja: si la distancia entre los átomos i y j es mayor que L/2, 
+            //entonces por condiciones periodicas, la distancia más corta entre los átomos i y j es L - r_ij, 
+            //donde r_ij es la distancia entre los átomos i y j sin tener en cuenta las condiciones periódicas de la caja
+            
+            double x_ij = x[i] - x[j];
+            if (x_ij > L / 2.0)  x_ij -= L;
+            if (x_ij < -L / 2.0)  x_ij += L;
+
+            double y_ij = y[i] - y[j];
+            if (y_ij > L / 2.0) y_ij -= L;
+            if (y_ij < -L / 2.0) y_ij += L;
+
+            //Distancia entre los átomos i y j, teniendo en cuenta las condiciones periódicas de la caja.
+            double r_ij = sqrt(x_ij * x_ij + y_ij * y_ij);
+
+            // Si la distancia es mayor que 3.0, la iteracción entre los atmos es nula 
+            if ((r_ij > 0.0) && (r_ij < 3.0)) 
+            {
+                double inv_r_ij = 1.0 / r_ij;
+
+                double term = 24.0 * (2.0 * pow(inv_r_ij, 14) - pow(inv_r_ij, 8));
+
+                a_x[i] += term * x_ij; 
+                a_y[i] += term * y_ij;
+
+                // Por la tercera ley de Newton, la fuerza que el átomo j ejerce sobre el átomo i es igual y opuesta a la fuerza que el átomo i ejerce sobre el átomo j
+                a_x[j] -= term * x_ij;
+                a_y[j] -= term * y_ij;
+            }
+        }
+    }
+}
